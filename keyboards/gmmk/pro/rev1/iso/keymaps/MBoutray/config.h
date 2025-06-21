f5e87e68e5971ae6936b23b5b5058713901c39fd
MBoutray <martindeboutray@hotmail.fr>
2025-06-21 10:39:50 +0200

    reorder config; add flags for enabling functionalities

--- keyboards/gmmk/pro/rev1/iso/keymaps/MBoutray/config.h
+++ keyboards/gmmk/pro/rev1/iso/keymaps/MBoutray/config.h
@@ -32,68 +32,13 @@
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 
+/* Unicode input configuration */
+#define UNICODE_KEY_WINC KC_RIGHT_ALT
+
 /* Auto Shift Configuration */
 #define AUTO_SHIFT_TIMEOUT 175
 #define NO_AUTO_SHIFT_NUMERIC
 #define NO_AUTO_SHIFT_SPECIAL
-
-/* RGB Matrix Configuration */
-#ifdef RGB_MATRIX_ENABLE
-    #define RGB_MATRIX_KEYPRESSES
-    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-    #define RGB_DISABLE_TIMEOUT 1200000              // 20 minutes
-    #define RGB_DISABLE_WHEN_USB_SUSPENDED
-    #define RGB_MATRIX_LED_FLUSH_LIMIT 16
-    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150        // Limit brightness to reduce power consumption
-    #define RGB_MATRIX_HUE_STEP 8
-    #define RGB_MATRIX_SAT_STEP 8
-    #define RGB_MATRIX_VAL_STEP 8
-    #define RGB_MATRIX_SPD_STEP 10
-
-    /* Enable specific RGB effects */
-    #define ENABLE_RGB_MATRIX_BREATHING
-    #define ENABLE_RGB_MATRIX_CYCLE_ALL
-    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-    #define ENABLE_RGB_MATRIX_SPLASH
-    #define ENABLE_RGB_MATRIX_MULTISPLASH
-    #define ENABLE_RGB_MATRIX_SOLID_SPLASH
-    #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-
-    /* Disable unused RGB effects to save space */
-    #define DISABLE_RGB_MATRIX_ALPHAS_MODS
-    #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-    #define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-    #define DISABLE_RGB_MATRIX_BAND_SAT
-    #define DISABLE_RGB_MATRIX_BAND_VAL
-    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-    #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-    #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN
-    #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-    #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN
-    #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-    #define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL
-    #define DISABLE_RGB_MATRIX_CYCLE_SPIRAL
-    #define DISABLE_RGB_MATRIX_DUAL_BEACON
-    #define DISABLE_RGB_MATRIX_RAINBOW_BEACON
-    #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-    #define DISABLE_RGB_MATRIX_RAINDROPS
-    #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-    #define DISABLE_RGB_MATRIX_HUE_BREATHING
-    #define DISABLE_RGB_MATRIX_HUE_PENDULUM
-    #define DISABLE_RGB_MATRIX_HUE_WAVE
-    #define DISABLE_RGB_MATRIX_TYPING_HEATMAP
-    #define DISABLE_RGB_MATRIX_DIGITAL_RAIN
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#endif
 
 /* Tap Dance Configuration */
 #define TAPPING_TERM 200
@@ -121,9 +66,77 @@
 
 /* Custom Feature Flags */
 #define VIM_NAVIGATION_ENABLE
-#define FRENCH_ACCENTS_ENABLE
-#define LAYOUT_SWITCHER_ENABLE
+#define AZERTY_AFNOR_ENABLE
+#define QWERTY_ENABLE
+#define BEPO_ENABLE
 #define SMART_BRACKETS_ENABLE
 
-/* Unicode input configuration */
-#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_WINCOMPOSE
+/* RGB Matrix Configuration */
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_KEYPRESSES
+    // #define RGB_MATRIX_KEYRELEASES                                       // reactive effects respond to keyreleases (instead of keypresses)
+    #define RGB_MATRIX_TIMEOUT 1200000 // 20 minutes                     // number of milliseconds to wait until rgb automatically turns off
+    #define RGB_MATRIX_SLEEP                                             // turn off effects when suspended
+    #define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5  // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+    #define RGB_MATRIX_LED_FLUSH_LIMIT 16                                // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+    #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150                            // limits maximum brightness of LEDs to 200 out of 255. If not defined maximum brightness is set to 255
+    #define RGB_MATRIX_DEFAULT_ON true                                   // Sets the default enabled state, if none has been set
+    #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT          // Sets the default mode, if none has been set
+    #define RGB_MATRIX_DEFAULT_HUE 0                                     // Sets the default hue value, if none has been set
+    #define RGB_MATRIX_DEFAULT_SAT 255                                   // Sets the default saturation value, if none has been set
+    #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS         // Sets the default brightness value, if none has been set
+    #define RGB_MATRIX_DEFAULT_SPD 127                                   // Sets the default animation speed, if none has been set
+    #define RGB_MATRIX_HUE_STEP 8                                        // The value by which to increment the hue per adjustment action
+    #define RGB_MATRIX_SAT_STEP 16                                       // The value by which to increment the saturation per adjustment action
+    #define RGB_MATRIX_VAL_STEP 16                                       // The value by which to increment the brightness per adjustment action
+    #define RGB_MATRIX_SPD_STEP 16                                       // The value by which to increment the animation speed per adjustment action
+    #define RGB_MATRIX_DEFAULT_FLAGS LED_FLAG_ALL                        // Sets the default LED flags, if none has been set
+    #define RGB_TRIGGER_ON_KEYDOWN                                       // Triggers RGB keypress events on key down. This makes RGB control feel more responsive. This may cause RGB to not function properly on some boards
+
+    /* Enable/Disable specific RGB effects */
+    #define ENABLE_RGB_MATRIX_ALPHAS_MODS
+    #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+    #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+    #define ENABLE_RGB_MATRIX_BREATHING
+    #define ENABLE_RGB_MATRIX_BAND_SAT
+    #define ENABLE_RGB_MATRIX_BAND_VAL
+    #define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+    #define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+    #define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+    #define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+    #define ENABLE_RGB_MATRIX_CYCLE_ALL
+    #define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+    #define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+    #define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+    #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+    #define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+    #define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+    #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+    #define ENABLE_RGB_MATRIX_DUAL_BEACON
+    #define ENABLE_RGB_MATRIX_RAINBOW_BEACON
+    #define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+    #define ENABLE_RGB_MATRIX_RAINDROPS
+    #define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+    #define ENABLE_RGB_MATRIX_HUE_BREATHING
+    #define ENABLE_RGB_MATRIX_HUE_PENDULUM
+    #define ENABLE_RGB_MATRIX_HUE_WAVE
+    #define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
+    #define ENABLE_RGB_MATRIX_PIXEL_FLOW
+    #define ENABLE_RGB_MATRIX_PIXEL_RAIN
+    // Framebuffer defines (additional logic can increase firmware size)
+    #define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+    #define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+    // Reactive defines (additional logic can increase firmware size)
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+    #define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+    #define ENABLE_RGB_MATRIX_SPLASH
+    #define ENABLE_RGB_MATRIX_MULTISPLASH
+    #define ENABLE_RGB_MATRIX_SOLID_SPLASH
+    #define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
