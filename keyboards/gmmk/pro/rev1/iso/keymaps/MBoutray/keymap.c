15658604c9d2a87b2fd93b5f2d0424a1a8a19a99
MBoutray <martindeboutray@hotmail.fr>
2025-07-02 20:57:26 +0200

    change shift position in keymap

--- keyboards/gmmk/pro/rev1/iso/keymaps/MBoutray/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/MBoutray/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         FR_SUP2, FR_AMPR, FR_EACU, FR_DQUO, FR_QUOT, FR_LPRN, FR_MINS, FR_EGRV, FR_UNDS, FR_CCED, FR_AGRV, FR_RPRN, FR_EQL,  KC_BSPC,          KC_PSCR,
         KC_TAB,  FR_A,    FR_Z,    FR_E,    FR_R,    FR_T,    FR_Y,    FR_U,    FR_I,    FR_O,    FR_P,    FR_CIRC, FR_DLR,                    KC_INS,
         LT_NAVI, FR_Q,    FR_S,    FR_D,    FR_F,    FR_G,    FR_H,    FR_J,    FR_K,    FR_L,    FR_M,    FR_UGRV, FR_ASTR, KC_ENT,           KC_PGUP,
-        KC_LSFT, FR_LABK, FR_W,    FR_X,    FR_C,    FR_V,    FR_B,    FR_N,    FR_COMM, FR_SCLN, FR_COLN, FR_EXLM,          KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LSFT, FR_LABK, FR_W,    FR_X,    FR_C,    FR_V,    FR_B,    FR_N,    FR_COMM, FR_SCLN, FR_COLN, FR_EXLM, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, MO_FUNC, KC_LALT,                            KC_SPC,                             KC_RALT, TD_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /* AZERTY Afnor base layer*/
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         AF_AT,   AF_AGRV, AF_EACU, AF_EGRV, AF_ECIR, AF_LPRN, AF_RPRN, AF_LSQU, AF_RSQU, AF_LDAQ, AF_RDAQ, AF_QUOT, AF_CIRC, KC_BSPC,          KC_PSCR,
         KC_TAB,  AF_A,    AF_Z,    AF_E,    AF_R,    AF_T,    AF_Y,    AF_U,    AF_I,    AF_O,    AF_P,    AF_MINS, AF_PLUS,                   KC_INS,
         LT_NAVI, AF_Q,    AF_S,    AF_D,    AF_F,    AF_G,    AF_H,    AF_J,    AF_K,    AF_L,    AF_M,    AF_SLSH, AF_ASTR, KC_ENT,           KC_PGUP,
-        KC_LSFT, AF_LABK, AF_W,    AF_X,    AF_C,    AF_V,    AF_B,    AF_N,    AF_DOT,  AF_COMM, AF_COLN, AF_SCLN,          KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LSFT, AF_LABK, AF_W,    AF_X,    AF_C,    AF_V,    AF_B,    AF_N,    AF_DOT,  AF_COMM, AF_COLN, AF_SCLN, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, MO_FUNC, KC_LALT,                            KC_SPC,                             KC_RALT, TD_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /* QWERTY base layer*/
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PSCR,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_INS,
         LT_NAVI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGUP,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, MO_FUNC, KC_LALT,                            KC_SPC,                             KC_RALT, TD_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /* BEPO base layer*/
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_PSCR,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_INS,
         LT_NAVI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGUP,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, MO_FUNC, KC_LALT,                            KC_SPC,                             KC_RALT, TD_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /* Function layer */
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_F23,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_F24,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_HOME,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_END,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_END,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
     /* Navigation layer - VIM-like cursor movement */
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, VIM_W,   VIM_E,   _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, VIM_GG,  VIM_H,   VIM_J,   VIM_K,   VIM_L,   VIM_0,   VIM_DLR, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, VIM_B,   _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, VIM_B,   _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
     /* Symbols Layer - Programming & French Accents */
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______,       _______,     _______,          _______,
         _______, _______,    _______, _______, FR_EACU, _______, _______, _______, _______, _______,     _______, SMART_BRACKET, SMART_BRACE,                   _______,
         _______, _______,    _______, FR_EGRV, _______, _______, _______, _______, _______, _______,     _______, SMART_QUOTE,   _______,     _______,          _______,
-        _______, _______,    _______, _______, _______, _______, _______, _______, _______, SMART_ANGLE, _______, _______,                    _______, _______, _______,
+        _______, _______,    _______, _______, _______, _______, _______, _______, _______, SMART_ANGLE, _______, _______,       _______,              _______, _______,
         _______, _______,    _______,                            _______,                                _______, _______,       _______,     _______, _______, _______
     ),
     /* Numpad Layer */
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PSLS, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PAST, _______, _______,                   TG_NUMP,
         _______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PMNS, _______, _______, KC_PENT,          _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, KC_PPLS, _______,          _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_P0,   KC_PDOT, KC_PPLS, _______, _______,          _______, _______,
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
     ),
     /* Gaming Layer - No custom keys */
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_INS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   TG_GAME,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGUP,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
     /* System Layer - System controls and utilities */
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, LAY_AZE_STD, LAY_AZE_AFN, LAY_QWE, LAY_BPO, TO_GAME, TO_NUMP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,          XXXXXXX,
         XXXXXXX, XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   TG_SYST,
         XXXXXXX, XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
-        XXXXXXX, XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,
         XXXXXXX, MO_RGB,     XXXXXXX,                                XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     /* RGB Layer */
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RM_ALPHAS_MODS,            RM_GRAD_UP_DOWN,           RM_GRAD_LEFT_RIGHT,        RM_BREATHING,       RM_BAND_SAT,     RM_BAND_VAL,       RM_BAND_PINWHEEL_SAT, RM_BAND_PINWHEEL_VAL,  RM_BAND_SPIRAL_SAT,   RM_BAND_SPIRAL_VAL,  RM_CYC_ALL,               RM_CYC_LEFT_RIGHT,    _______,          _______,
         _______, RM_CYC_UP_DOWN,            RM_RAINBOW_MOVING_CHEVRON, RM_CYC_OUT_IN,             RM_CYC_OUT_IN_DUAL, RM_CYC_PINWHEEL, RM_CYC_SPIRAL,     RM_DUAL_BEACON,       RM_RAINBOW_BEACON,     RM_RAINBOW_PINWHEELS, RM_RAINDROPS,        RM_JELLYBEAN_RAINDROPS,   RM_HUE_BREATHING,                       _______,
         _______, RM_HUE_PENDULUM,           RM_HUE_WAVE,               RM_PIX_FRACTAL,            RM_PIX_FLOW,        RM_PIX_RAIN,     RM_TYPING_HEATMAP, RM_DIGITAL_RAIN,      RM_SOLID_REACT_SIMPLE, RM_SOLID_REACT,       RM_SOLID_REACT_WIDE, RM_SOLID_REACT_MULTIWIDE, RM_SOLID_REACT_CROSS, _______,          _______,
-        _______, RM_SOLID_REACT_MULTICROSS, RM_SOLID_REACT_NEXUS,      RM_SOLID_REACT_MULTINEXUS, RM_SPLASH,          RM_MULTISPLASH,  RM_SOLID_SPLASH,   RM_SOLID_MULTISPLASH, _______,               _______,              _______,             _______,                                        _______, _______, _______,
+        _______, RM_SOLID_REACT_MULTICROSS, RM_SOLID_REACT_NEXUS,      RM_SOLID_REACT_MULTINEXUS, RM_SPLASH,          RM_MULTISPLASH,  RM_SOLID_SPLASH,   RM_SOLID_MULTISPLASH, _______,               _______,              _______,             _______,                  _______,                       _______, _______,
         _______, _______,                   _______,                                                                                   _______,                                                                              _______,             _______,                  _______,              _______, _______, _______
     )
 };
